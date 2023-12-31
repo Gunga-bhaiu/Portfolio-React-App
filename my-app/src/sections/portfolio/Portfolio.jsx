6926f5bce67a319b2b857dd086609a474f55afd4
unityDevz <139897881+unityDevz@users.noreply.github.com>
2023-12-31 16:27:38 +0530

    commited new changes

--- my-app/src/sections/portfolio/Portfolio.jsx
+++ my-app/src/sections/portfolio/Portfolio.jsx
@@ -1,40 +1,33 @@
 import React from 'react';
 import "./portfolio.css";
-import buttonsData from './butttonsData';
 import cardData from './cardsData.js';
-import {frontend , all , backend , mulesoft} from './cardsData';
-import Projects from '../../components/project.jsx'
 import Card from '../../components/card.jsx'
 import { useState } from 'react';
 
 const ids = 1;
 
 const Portfolio = () => {
+    let [isActive, setIsToggeled] = useState("All");
     return (
         <section id="portfolio">
             <h2>Recent Projects</h2>
             <div className='container'>
                 <div className='portfolio__container'>
-                {
-                            buttonsData.map(item => (
-                                <Projects key={item.id} uniqueId={item.icon} className='btn primary' flag={item.flag}>
-                                    <span className='portfolio__button-icon'>{item.icon}</span>
-                                    <h5>{item.title}</h5>
-                                    {item.flag ? <p>Agya Mein Bhai</p> : ""}
-                                </Projects> 
-                            ))
-                        }
+                    <a className={` ${isActive === "All" ? 'portfolio__container__items btn primary' : 'portfolio__container__items btn'}`} onClick={() => setIsToggeled("All")}>All</a>
+                    <a className={` ${isActive === "Frontend" ? 'portfolio__container__items btn primary' : 'portfolio__container__items btn'}`} onClick={() => setIsToggeled("Frontend")}>Frontend</a>
+                    <a className={` ${isActive === "Backend" ? 'portfolio__container__items btn primary' : 'portfolio__container__items btn'}`} onClick={() => setIsToggeled("Backend")}>Backend</a>
+                    <a className={` ${isActive === "Mulesoft" ? 'portfolio__container__items btn primary' : 'portfolio__container__items btn'}`} onClick={() => setIsToggeled("Mulesoft")}>Mulesoft</a>
                 </div>
                 <div className='portfolio__container'>
-                {
-                            cardData.map(item => (
-                <Card key={item.id} className='portfolio__card-items'>
-                                    <span className='portfolio__card-icon'>{item.icon}</span>
-                                    <h5>{item.title}</h5>
-                                    <small>{item.des}</small>
-                                </Card>
-                                ))
-                            }
+                    {
+                        cardData.map(item => (
+                            <Card key={item.id} className='portfolio__card-items'>
+                                <span className='portfolio__card-icon'>{item.icon}</span>
+                                <h5>{item.title}</h5>
+                                <small>{item.des}</small>
+                            </Card>
+                        ))
+                    }
                 </div>
             </div>
         </section>
--- my-app/src/sections/portfolio/butttonsData.js
+++ my-app/src/sections/portfolio/butttonsData.js
@@ -4,10 +4,10 @@ import { AiFillDatabase } from "react-icons/ai";
 
 
 const buttonsData = [
-    {id:1, link: '#' , icon: <TbAwardFilled/> , title: 'All' , flag: false},
-    {id:2, link: '#about' , icon: <GrProjects/> , title: 'Frontend' , flag: false},
-    {id:3, link: 'services' ,  icon: <AiFillDatabase/> , title: 'Backend' , flag: false},
-    {id:4, link: '#portfolio' ,  icon: <TbAwardFilled/> , title: 'Mulesoft' , flag: false}
+    {id:1, link: '#' , icon: <TbAwardFilled/> , title: 'All' , flag: "All"},
+    {id:2, link: '#about' , icon: <GrProjects/> , title: 'Frontend' , flag: "frontend"},
+    {id:3, link: 'services' ,  icon: <AiFillDatabase/> , title: 'Backend' , flag: "backend"},
+    {id:4, link: '#portfolio' ,  icon: <TbAwardFilled/> , title: 'Mulesoft' , flag: "mulesoft"}
 ]
 
 export default buttonsData;
